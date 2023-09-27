1b071ba50004f21603ce8a2b69e887d868529f90
kolepeoples <kolepeoples@gmail.com>
2023-09-27 13:03:37 -0500

    Finished Calculator

--- Program.cs
+++ Program.cs
@@ -1,4 +1,38 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Testing Git and output");
+using System;
+
+Console.WriteLine("Enter your first number: ");
+double firstNum = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Enter the operator (+, -, x/*, /):");
+string myOp = Console.ReadLine();
+if (myOp.Length > 1)
+{
+    Console.WriteLine("Enter a valid operator: ");
+    myOp = Console.ReadLine();
+}
+
+Console.WriteLine("Enter your second number: ");
+double secondNum = Convert.ToDouble(Console.ReadLine());
+
+if(myOp == "+")
+{
+    Console.WriteLine($"{firstNum} + {secondNum} = ", firstNum + secondNum);
+}
+
+if (myOp == "x" || myOp == "*")
+{
+    Console.WriteLine($"{firstNum} * {secondNum} = ", firstNum * secondNum);
+}
+if (myOp == "-")
+{
+    Console.WriteLine($"{firstNum} - {secondNum} = ", firstNum - secondNum);
+}
+if (myOp == "/")
+{
+    Console.WriteLine($"{firstNum} / {secondNum} = ", firstNum / secondNum);
+}
+
+
+
+
 
